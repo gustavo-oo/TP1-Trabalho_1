8702da741106bf5c225beef42c66893663a15858
gustavo-oo <50386763+gustavo-oo@users.noreply.github.com>
2020-09-29 11:43:51 -0300

    Revert "Documentação Codigo de Agencia até Data, pulando CPF"

--- doxygen/html/class_codigo_de_aplicacao.js
+++ doxygen/html/class_codigo_de_aplicacao.js
@@ -1,6 +1,5 @@
 var class_codigo_de_aplicacao =
 [
     [ "GetCodigoDeAplicacao", "class_codigo_de_aplicacao.html#a4a8ab0556fe8bba26d529bd21573347b", null ],
-    [ "SetCodigoDeAplicacao", "class_codigo_de_aplicacao.html#aa6b1eae89081718b8cb88f83af62b6f9", null ],
-    [ "ValidarCodigoDeAplicacao", "class_codigo_de_aplicacao.html#a9d56ade2dd62fa3c3b0ffeb623196c5f", null ]
+    [ "SetCodigoDeAplicacao", "class_codigo_de_aplicacao.html#aa6b1eae89081718b8cb88f83af62b6f9", null ]
 ];
--- doxygen/html/class_codigo_de_produto.js
+++ doxygen/html/class_codigo_de_produto.js
@@ -1,6 +1,5 @@
 var class_codigo_de_produto =
 [
     [ "GetCodigoDeProduto", "class_codigo_de_produto.html#a3b4bcd7b139a64da558aea867aba371a", null ],
-    [ "SetCodigoDeProduto", "class_codigo_de_produto.html#a0ac9a77cbe69d102404a22bbc6554445", null ],
-    [ "ValidarCodigoDeProduto", "class_codigo_de_produto.html#ae098823b76a32933b2d6f004230e2472", null ]
+    [ "SetCodigoDeProduto", "class_codigo_de_produto.html#a0ac9a77cbe69d102404a22bbc6554445", null ]
 ];
